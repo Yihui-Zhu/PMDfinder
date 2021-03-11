2f4f4a0ac5ee75fdfe51800bae723e0b1e641655
Yihui Zhu <43891200+Yihui-Zhu@users.noreply.github.com>
2021-03-10 21:14:06 -0800

    Create PMDfinder.py

---
+++ Python/PMDfinder.py
@@ -0,0 +1,125 @@
+# Load Packages
+import pandas as pd
+import numpy as np
+import random
+import sklearn
+from sklearn.model_selection import LeaveOneOut
+from sklearn import preprocessing
+from matplotlib import pyplot as plt
+
+# load DSS methylation data
+methylation = pd.read_csv("tables/dataset_meth.csv")
+
+# store the location and the percent methylation
+meth_ratio = methylation["methM"]/methylation["totalN"]
+geno_pos = methylation["pos"]
+location = methylation["number"]
+
+### Data Conversion
+# convert methylation ratio to PMD/non-PMD level (y=4x(1-x))
+def methRatio2PMDLevel(meth_ratio):
+    n = len(meth_ratio)
+    PMD_level = [0]*n
+    for i in range(n):
+        PMD_level[i] = 4 * meth_ratio[i] * (1 - meth_ratio[i])
+    return PMD_level
+
+PMD_level = methRatio2PMDLevel(meth_ratio)
+
+### Sequential Data Matrix
+# Extract sequenctial feature by sliding window
+N = len(PMD_level)
+X = np.zeros((N-1023, 1024))
+for i in range(N-1023):
+    X[i, :] = PMD_level[i:i+1024]
+
+### Autoencoder
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+
+# latent is the last variable
+latent_dim = 8
+
+class Autoencoder(Model):
+  def __init__(self, latent_dim):
+    super(Autoencoder, self).__init__()
+    self.latent_dim = latent_dim   
+    self.encoder = tf.keras.Sequential([
+      layers.Dense(latent_dim, activation=layers.LeakyReLU(alpha=0.3)),
+    ])
+    self.decoder = tf.keras.Sequential([
+       layers.Dense(1024, activation=layers.LeakyReLU(alpha=0.3))
+    ])
+
+  def call(self, x):
+    encoded = self.encoder(x)
+    decoded = self.decoder(encoded)
+    return decoded
+
+m = Autoencoder(latent_dim)
+m.compile(optimizer='adam', loss=losses.MeanSquaredError())
+
+# fit the model
+m.fit(X, X, epochs=5, shuffle=True)
+
+# get the encoded PMD
+encoded_slicing_PMD = m.encoder(X).numpy()
+
+### k-means
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=22).fit(encoded_slicing_PMD)
+final_result = kmeans.labels_
+
+### Post-processing steps
+## Remove PMD that is less than 51 bp length
+assign1 = [] # index for the location equal to 1
+for i in range(len(final_result)):
+    if final_result[i] == 1:
+        assign1.append(i)
+
+break_pts1 = [] # index for the break point, the next equal to 1 is more than 1bp
+for i in range(1, len(assign1)):
+    if assign1[i] - assign1[i-1] > 1:
+        break_pts1.append(i)    
+
+# small_PMD_intervals: identify region that is close with each other 
+small_PMD_intervals = []
+for i in range(1, len(break_pts1)):
+    if assign1[break_pts1[i]-1] - assign1[break_pts1[i-1]] + 1 < 101:
+        small_PMD_intervals.append(i)
+
+# change the PMD interval with less than 51 to Non-PMD
+for interval in small_PMD_intervals:
+    final_result[assign1[break_pts1[interval-1] : break_pts1[interval]-1]] == 0
+
+## Merge PMD that is less than 101 bp from the next one
+# This need to check the non-PMD region length
+assign2 = []
+for i in range(len(final_result)):
+    if final_result[i] == 0:
+        assign2.append(i)
+
+break_pts2 = []
+for i in range(1, len(assign2)):
+    if assign2[i] - assign2[i-1] > 1:
+        break_pts2.append(i)
+
+# small non_PMD intervals
+small_non_PMD_intervals = []
+for i in range(1, len(break_pts2)):
+    if assign2[break_pts2[i]-1] - assign2[break_pts2[i-1]] + 1 < 101:
+        small_non_PMD_intervals.append(i)
+
+# change the PMD interval with less than 51 to Non-PMD
+for interval in small_non_PMD_intervals:
+    final_result[assign2[break_pts2[interval-1] : break_pts2[interval]-1]] == 0
+
+np.savetxt('tables/final_result.csv', final_result, delimiter=',')
